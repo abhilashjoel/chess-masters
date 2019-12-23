12ad9048d50c1f3705baa1cc2a41600fd8e34167
Joel <abhilash.yovan@freshworks.com>
2019-12-24 01:53:41 +0530

    initial commit

---
+++ src/com/chessmasters/DummyServlet.java
@@ -0,0 +1,22 @@
+package com.chessmasters;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/app")
+public class DummyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Chess Masters...");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: abhilashjy
+  Date: 24/12/19
+  Time: 1:07 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Chess Bot</title>
+  </head>
+  <body>
+  Chess Bot...
+  </body>
+</html>
