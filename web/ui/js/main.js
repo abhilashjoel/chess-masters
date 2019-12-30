115a1b7a96902c3fd1ff2ea37d77293702d3e6ac
Joel <abhilash.yovan@freshworks.com>
2019-12-31 02:23:01 +0530

    Chess piece movement logic

--- web/ui/js/main.js
+++ web/ui/js/main.js
@@ -30,6 +30,7 @@ class ChessBoard {
         ];
         //To track each tile on the board. Basically contains <div>'s
         this.tiles = Array(8);
+        this.tileIdPieceMap = Array(64);
     }
 }
 
@@ -37,7 +38,7 @@ class ChessBoard {
 function initChessTable() {
     let tiles = globals.chessBoard.tiles;
     let grid = document.getElementById("chess-board");
-    let i = 1;
+    let i = 0;
     for(let x = 0; x < 8; x++) {
         tiles[x] = Array(8);
         for(let y = 0; y < 8; y++) {
@@ -56,98 +57,82 @@ function initChessTable() {
 
 
 
-            console.log(x, y, tile.id)
+            console.log(x, y, tile.id);
             grid.appendChild(tile);
-            tile = document.getElementById(i-1)
-            if(x > 5){
-                tile.draggable = true;
-                tile.setAttribute("onDragStart", "drag(event)");
-                tile.setAttribute("dragEnd", "allowDrop(event)");
-            } else {
-                tile.setAttribute("onDrop", "drop(event)");
-                tile.setAttribute("onDragEnter", "allowDrop(event)");
-                tile.setAttribute("onDragOver", "allowDrop(event)");
-                tile.setAttribute("dragleave", "allowDrop(event)");
-                tile.setAttribute("dragend", "allowDrop(event)");
-            }
-
-
+            tile.draggable = true;
+            tile.setAttribute("onDragStart", "drag(event, " + tile.id +")");
+            tile.setAttribute("onDrop", "drop(event)");
+            tile.setAttribute("onDragEnter", "allowDrop(event)");
+            tile.setAttribute("onDragOver", "allowDrop(event)");
+            tile.setAttribute("dragleave", "allowDrop(event)");
+            tile.setAttribute("dragend", "allowDrop(event)");
         }
     }
 }
 
 function allowDrop(ev) {
     ev.preventDefault();
-    console.log("allowDrop")
   }
   
 
 function showChessMen() {
     let chessMen = globals.chessBoard.board;
+    let tileIdPieceMap = globals.chessBoard.tileIdPieceMap;
     console.log(chessMen);
     let tiles = globals.chessBoard.tiles;
+    let id = 0;
     for(let x = 0; x < 8; x++) {
         for(let y = 0; y < 8; y++) {
             if(chessMen[x][y]){
-                let chessPiece = document.createElement("div");
-                console.log("Appending ", x, y, chessMen[x][y], tiles[x][y].id);
-                chessPiece.classList.add(chessMen[x][y]);
                 tiles[x][y].classList.add(chessMen[x][y]);
-//                tiles[x][y].appendChild(chessPiece);
             }
+            tileIdPieceMap[id] = chessMen[x][y];
+            id++;
+            
         }
     }
 }
 
 
-var drag = function(ev) {
-    ev.preventDefault();
-    console.log("Drag");
-    ev.dataTransfer.setData("tile-id", ev.target.id);
+var drag = function(event, id) {
+    console.log("Drag: ", id);
+    event.dataTransfer.setData("tile-id", id);
+    if(!globals.chessBoard.tileIdPieceMap[id]) {
+        console.log("Looks like this tile is empty...");
+    }
 }
   
-var drop = function(ev) {
-    ev.preventDefault();
+var drop = function(event) {
+    event.preventDefault();
+    let tileIdPieceMap =  globals.chessBoard.tileIdPieceMap;
+    let board = globals.chessBoard.board;
     console.log("drop")
-    let tileId = ev.dataTransfer.getData("tile-id");
-    let x = Math.floor(tileId / 8);
-    let y = tileId * 8;
-    console.log(tileId, x, y);
-//    ev.target.appendChild(document.getElementById(data));
+    let sourceTileId = event.dataTransfer.getData("tile-id");
+    let destTile = event.target;
+    let destTileId = destTile.id;
+    console.log("source: ", sourceTileId)
+    console.log("dest", destTileId);
+    let sourceTile = document.getElementById(sourceTileId);
+    sourceTile.classList.remove(tileIdPieceMap[sourceTileId])
+    if(tileIdPieceMap[destTileId]){
+        destTile.classList.remove(tileIdPieceMap[destTileId]);
+    }
+    destTile.classList.add(tileIdPieceMap[sourceTileId]);
+    tileIdPieceMap[destTileId] = tileIdPieceMap[sourceTileId]
+    tileIdPieceMap[sourceTileId] = "";
+
+    let x1 = Math.floor(sourceTileId / 8);
+    let y1 = sourceTileId % 8;
+    let x2 = Math.floor(destTileId / 8);
+    let y2 = destTileId % 8;
+
+    board[x2][y2] = board[x1][y1];
+    board[x1][y1] = "";
+    console.log(board);
   }
 
 
 globals.chessBoard = new ChessBoard()
 
-//initChessTable();
-//showChessMen();
-
-
-// document.addEventListener("dragstart", (event) => { 
-//     event.preventDefault();
-//     event.dataTransfer.setData("x", "y");
-//     console.log("dragStart")
-// });
-// document.addEventListener("dragleave", (event) => {
-//     event.preventDefault();
-//     console.log("dragLeave")
-// });
-
-// grid.addEventListener("drop", (event) => {
-//     event.preventDefault();
-//     console.log("drop")
-// });
-
-// document.addEventListener("dragend", (event) => { 
-//     event.preventDefault();
-//     console.log("dragEnd")
-// });
-// grid.addEventListener("dragenter", (event) => { 
-//     event.preventDefault();
-//     console.log("dragEnter")
-// });
-// grid.addEventListener("dragover", (event) => {
-//      event.preventDefault();
-//     console.log("dragOVer")
-// });
-
+initChessTable();
+showChessMen();
